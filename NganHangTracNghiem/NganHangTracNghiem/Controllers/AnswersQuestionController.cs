1bfcf98ced7cfa73f49b02e03722d8bd40163706
nguyentanloc200495 <nguyentanloc.200495@gmail.com>
2017-06-26 16:16:25 +0700

    a
    a

---
+++ NganHangTracNghiem/NganHangTracNghiem/Controllers/AnswersQuestionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NganHangTracNghiem.Models;
+
+namespace NganHangTracNghiem.Controllers
+{
+    public class AnswersQuestionController : ApiController
+    {
+        ObjectiveTestEntities db = new ObjectiveTestEntities();
+        public IHttpActionResult GetAnswersQuestion(int id)
+        {
+            try
+            {
+                db.Configuration.ProxyCreationEnabled = false;
+                var answers = db.Answers.Where(n => n.QuestionId == id);
+                return Ok(answers);
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
